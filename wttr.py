a1750b1d4d09b4c5563094080f7fc9f4de50066d
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-08-01 21:33:22 +0100

    chore: fix typo

--- wttr.py
+++ wttr.py
@@ -39,7 +39,7 @@ async def get_city_weather(city: str) -> str:
     return await get_weather(city)
 
 
-@mcp.prompt("ron_weather", title="Get weather as Run Burgundy")
+@mcp.prompt("ron_weather", title="Get weather as Ron Burgundy")
 def ron_weather(city: str) -> str:
     """Prompt to get weather for a specific city in the style of Ron Burgundy.
 
