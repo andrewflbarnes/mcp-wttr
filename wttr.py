9774f4e18aeb8e94d4048d6187b6efced11f26c8
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-08-01 21:29:01 +0100

    chore: formatting

--- wttr.py
+++ wttr.py
@@ -8,24 +8,23 @@ client = httpx.Client(verify=ssl)
 
 
 async def get_weather(city: str) -> str:
-    url = (f"http://wttr.in/{city}?format="
-           "Temperature=%t\\n"
-           "Wind=%w\\n"
-           "Humidity=%h\\n"
-           "Pressure=%P\\n"
-           "Precipitation=%p\\n"
-           "Weather=%C\\n"
-           "UV%20Index=%u"
-           )
+    url = (
+        f"http://wttr.in/{city}?format="
+        "Temperature=%t\\n"
+        "Wind=%w\\n"
+        "Humidity=%h\\n"
+        "Pressure=%P\\n"
+        "Precipitation=%p\\n"
+        "Weather=%C\\n"
+        "UV%20Index=%u"
+    )
 
     try:
         response = client.get(url)
         response.raise_for_status()
         return response.text.strip()
     except httpx.HTTPStatusError as e:
-        return (f"Error fetching weather: {e.response.status_code}"
-                f" - {e.response.text}"
-                )
+        return f"Error fetching weather: {e.response.status_code} - {e.response.text}"
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
@@ -47,9 +46,10 @@ def ron_weather(city: str) -> str:
     Args:
         city (str): The name of the city to get the weather for.
     """
-    return (f"Get weather for {city}."
-            "Present a summary in the style of Ron Burgundy from Anchorman."
-            )
+    return (
+        f"Get weather for {city}."
+        "Present a summary in the style of Ron Burgundy from Anchorman."
+    )
 
 
 if __name__ == "__main__":
